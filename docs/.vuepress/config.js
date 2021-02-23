ab8a7d7efb701d21d2783f5c9273972c1a9d3c7b
chenyilong369 <1456014766@qq.com>
2021-02-24 00:40:19 +0800

    dp

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  plugins: [
+    'latex' // or 'vuepress-plugin-latex'
+  ],
   base: '/',
   dest: 'dist',
   title: 'jschen Blog',
@@ -6,28 +9,53 @@ module.exports = {
   themeConfig: {
     editLinks: false,
     docsDir: 'docs',
-    nav: [],
-    sidebar: [
-      {
-        title: 'js基础',
-        collapsable: false,
-        children: [
-          'js01/type',
-          'js01/this',
-          'js01/scope',
-          'js01/prototype',
-          'js01/callStack',
-          
-        ]
-      },
-      {
-        title: 'V8',
-        collapsable: false,
-        children: [
-          'V8/storeAndDelete',
-          'V8/Compiler'
-        ]
-      }
-    ]
+    smoothScroll: true,
+    nav: [
+      {text: '前端', link: '/webDesign/'},
+      {text: '算法', link: '/algorithm/'},
+      {text: 'Github', link: 'https://github.com/chenyilong369'}
+    ],
+    sidebar: {
+      '/webDesign/': getWebDesign(),
+      '/algorithm/': getAlgroithm()
+    }
   }
+}
+
+function getWebDesign() {
+  return [
+    {
+      title: 'js基础',
+      collapsable: false,
+      children: [
+        'js01/type',
+        'js01/this',
+        'js01/scope',
+        'js01/prototype',
+        'js01/callStack',
+        
+      ]
+    },
+    {
+      title: 'V8',
+      collapsable: false,
+      children: [
+        'V8/storeAndDelete',
+        'V8/Compiler'
+      ]
+    },
+  ]
+}
+
+function getAlgroithm() {
+  return [
+    {
+      title: 'DP',
+      collapsable: false,
+      children: [
+        'dp/validity',
+        'dp/base',
+      ]
+    }
+  ]
 }
--- docs/README.md
+++ docs/README.md
@@ -2,5 +2,11 @@
 home: true
 heroImage: /logo.png
 actionText: 开始学习 →
-actionLink: /js01/
+actionLink: /webDesign/
+features:
+- title: 个人笔记
+  details: 个人下学习过程中记录下来的笔记，可以提供参考。
+- title: 算法
+  details: 享受算法，享受刷题的快乐吧
+footer: MIT Licensed | jsChen © 2021-present
 ---
